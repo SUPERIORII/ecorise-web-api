25c459ce87e7cc8027e55928486b144ae1599674
SUPERIORII <adukuly461@gmail.com>
2026-01-16 11:44:14 +0000

    joi password complexity reduced

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -3,5 +3,5 @@ import passwordComplexity from "joi-password-complexity";
 
 export const loginSchema = Joi.object({
   email: Joi.string().email().required().label("email"),
-  password: passwordComplexity().required().label("password"),
+  password: Joi.string().min(8).required().label("password"),
 });
